de399a15f326995209e487776a7814e7bcc35591
Jackson Dahl <jackson.dahl@gomboc.ai>
2025-02-07 15:42:44 -0500

    initialized client

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,4 +1,31 @@
 // Centralized API client (axios/fetch wrapper)
 import * as vscode from 'vscode';
+import logger from '../utils/logger';
+import settings from '../settings';
+import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
 
-export function initializeApiClient(context: vscode.ExtensionContext) {}
+export class CustomerApiClient {
+  private client;
+
+  constructor() {
+
+    const _settings = settings();
+
+    if (_settings instanceof Error) {
+      logger.error('Invalid settings', _settings);
+      throw new Error(_settings.message);
+    }
+
+    this.client = new ApolloClient({
+      ssrMode: true,
+      link: createHttpLink({
+        uri: `${_settings.CUSTOMER_API_URL}/graphql`,
+        credentials: 'same-origin',
+        headers: {
+          bazinga: 'true',
+        },
+      }),
+      cache: new InMemoryCache(),
+    });
+  }
+}
