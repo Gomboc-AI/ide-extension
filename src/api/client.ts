722eee7302762a61464e3dbfe13375349504a33c
justinkim <jkim@gomboc.ai>
2025-06-06 16:19:17 -0400

    style: linting

--- src/api/client.ts
+++ src/api/client.ts
@@ -78,7 +78,7 @@ export class CustomerApiClient {
       const { data } = await this.client.query<SecurityBenchmarksQuery>({
         query: SECURITY_BENCHMARKS,
       });
-      console.log('---securityAdoptedBenchmarkRecommendations', data)
+      console.log('---securityAdoptedBenchmarkRecommendations', data);
       logger.info('Fetched security benchmarks');
       const retval: SecurityBenchmarkQueryBenchmarkArray = [];
       for (const benchmark of data.securityBenchmarks) {
