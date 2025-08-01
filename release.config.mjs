ac04d6ddbe0864dfcc9f1c0ce3848730ae706e20
Gary <gchen1258@gmail.com>
2025-08-01 16:59:17 -0400

    Release 1.2.1
    chore(release): Releasing 1.2.1

--- release.config.mjs
+++ release.config.mjs
@@ -47,53 +47,17 @@ export default {
         },
       },
     ],
-    // [
-    //   '@semantic-release/commit-analyzer',
-    //   {
-    //     preset: 'angular',
-    //     releaseRules: [
-    //       { breaking: true, release: 'major' },
-    //       { revert: true, release: 'patch' },
-    //       { type: 'feat', release: 'minor' },
-    //       { type: 'fix', release: 'patch' },
-    //       { type: 'perf', release: 'patch' },
-    //       { type: 'revert', release: 'patch' },
-    //       { type: 'docs', scope: 'README', release: 'patch' },
-    //       { scope: 'release', release: 'patch' },
-    //       { scope: 'no-release', release: false },
-    //       { scope: 'release', release: 'patch' },
-    //     ],
-    //   },
-    // ],
-    // [
-    //   '@semantic-release/release-notes-generator',
-    //   {
-    //     preset: 'angular',
-    //     releaseRules: [
-    //       { breaking: true, release: 'major' },
-    //       { revert: true, release: 'patch' },
-    //       { type: 'feat', release: 'minor' },
-    //       { type: 'fix', release: 'patch' },
-    //       { type: 'perf', release: 'patch' },
-    //       { type: 'revert', release: 'patch' },
-    //       { type: 'docs', scope: 'README', release: 'patch' },
-    //       { scope: 'release', release: 'patch' },
-    //       { scope: 'no-release', release: false },
-    //       { scope: 'release', release: 'patch' },
-    //     ],
-    //   },
-    // ],
-    // [
-    //   'semantic-release-vsce',
-    //   {
-    //     packageVsix: true,
-    //   },
-    // ],
-    // [
-    //   '@semantic-release/github',
-    //   {
-    //     assets: '*.vsix',
-    //   },
-    // ],
+    [
+      'semantic-release-vsce',
+      {
+        packageVsix: true,
+      },
+    ],
+    [
+      '@semantic-release/github',
+      {
+        assets: '*.vsix',
+      },
+    ],
   ],
 };
