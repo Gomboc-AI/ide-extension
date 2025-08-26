8732b60bad2022ca2cb8c97571f521822f550ad8
Justin Kim <39386551+JustinKim3420@users.noreply.github.com>
2025-08-26 10:38:07 -0400

    Resolve discrepancy (#46)
    * fix: issues with custom policies
    
    * refactor: format

--- src/providers/scanResultsProvider.ts
+++ src/providers/scanResultsProvider.ts
@@ -91,6 +91,9 @@ export class ScanResultsProvider {
     for (const remediation of this.individualRemediations) {
       const filepath =
         remediation.codeObservation.codeResourceInstance.filepath;
+      if (remediation.fixes.length === 0) {
+        continue;
+      }
       const existingData = existingResourceBenchmarkFixes[filepath];
       if (!existingData) {
         existingResourceBenchmarkFixes[filepath] = [remediation];
