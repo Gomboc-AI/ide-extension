87f8d89ef0b70b76723821d6d7fbde330a45d6ee
Derek <derek@gonboc.ai>
2025-11-24 14:45:38 -0500

    feat: DEV-3500 cut out extra numbers used in testing

--- src/commands/testOrlConnection.ts
+++ src/commands/testOrlConnection.ts
@@ -35,7 +35,7 @@ export async function testOrlConnectionCommand(
         if (isConnected) {
           vscode.window
             .showInformationMessage(
-              '01010-ORL connection test successful',
+              'ORL connection test successful',
               'Open Settings',
             )
             .then(selection => {
